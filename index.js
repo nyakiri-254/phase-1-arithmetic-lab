2dea9e5ceac038f06527a23145a3302d280ce792
nyakiri_margaret <margaret.karanja@student.moringaschool.com>
2022-11-28 12:29:30 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const multiply = (num1,num2) =>{
+    return num1*num2;
+}
+let result = multiply(31*2);
+console.log(result);
+//62 
 
-// Write your code here
+const random = (Math.random()*(1>0));
+console.log ('Random integer greater than 0 is ${random}');
+
+const mod =(num3,num4);
+const remainder = 16%4; 
+console.log (remainder);
+
+//4
+
+const max =math.max(11,12,20);
+console.log(max);
